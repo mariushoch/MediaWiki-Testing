b049a2d665f9bae210728d0c2a9e64d896f1abfb
Marius Hoch <hoo@online.de>
2013-02-26 22:53:00 +0100

    Ignore .gitignore and Config.php

--- Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * User configuration, overrides the DefaultConfig.php
- * See DefaultConfig.php for available configuration options
- */
-
-// Absolute path to the folder which holds
-// all git repos for MediaWiki and extensions
-$mwtGitPath = '/home/hoch_m/wikimedia-git-repos';
