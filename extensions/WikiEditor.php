e85c72c64e5fa7762cce820b9d2c1356809f5e8c
Marius Hoch <hoo@online.de>
2013-10-07 11:17:43 +0200

    Add a couple of extensions

---
+++ extensions/WikiEditor.php
@@ -0,0 +1,24 @@
+<?php
+namespace mwt\extensions;
+use mwt\extension, mwt\Utilities;
+
+class WikiEditor extends extension {
+	/**
+	 * Constructor
+	 *
+	 * @param $wikis array Array with instance objects of wikis
+	 * 		this extension should be enabled on. Defaults to all wikis.
+	 */
+	public function __construct( $wikis = null ) {
+		parent::__construct( 'WikiEditor', $wikis );
+	}
+
+	/**
+	 * Get the name of the settings template file for this extension
+	 *
+	 * @return string
+	 */
+	public function getSettingsTemplate() {
+		return 'WikiEditor.php';
+	}
+}
